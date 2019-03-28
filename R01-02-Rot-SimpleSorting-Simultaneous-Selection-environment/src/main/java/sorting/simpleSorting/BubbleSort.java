3dbd3a113cfb640357b8d4e32474c68e104bd3f0
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2019-03-28 15:59:16 -0300

    Bla..

---
+++ R01-02-Rot-SimpleSorting-Simultaneous-Selection-environment/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * The bubble sort algorithm iterates over the array multiple times, pushing big
+ * elements to the right by swapping adjacent elements, until the array is
+ * sorted.
+ */
+public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+
+		boolean estaOrdenado = false;
+
+		while (!estaOrdenado) {
+
+			estaOrdenado = true;
+
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i].compareTo(array[i+1]) > 0) {
+					Util.swap(array, i, i+1);
+					estaOrdenado = false;
+				}
+			}
+
+			rightIndex--;
+
+		}
+
+	}
+}
---
+++ R01-02-Rot-SimpleSorting-Simultaneous-Selection-environment/src/main/java/sorting/variationsOfSelectionsort/SimultaneousSelectionsort.java
@@ -0,0 +1,45 @@
+package sorting.variationsOfSelectionsort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * This algorithm applies two selection sorts simultaneously. In a same
+ * iteration, a selection sort pushes the greatest elements to the right and
+ * another selection sort pushes the smallest elements to the left. At the end
+ * of the first iteration, the smallest element is in the first position (index
+ * 0) and the greatest element is the last position (index N-1). The next
+ * iteration does the same from index 1 to index N-2. And so on. The execution
+ * continues until the array is completely ordered.
+ */
+public class SimultaneousSelectionsort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+   public void sort(T[] array, int leftIndex, int rightIndex) {
+
+      while (leftIndex < rightIndex) {
+
+         int indiceMaior = leftIndex;
+         for (int i = leftIndex + 1; i <= rightIndex; i++) {
+            if (array[i].compareTo(array[indiceMaior]) > 0) {
+               indiceMaior = i;
+            }
+         }
+
+         Util.swap(array, rightIndex, indiceMaior);
+         rightIndex--;
+
+         int indiceMenor = rightIndex;
+         for (int i = rightIndex - 1; i >= leftIndex; i--) {
+            if (array[i].compareTo(array[indiceMenor]) < 0) {
+               indiceMenor = i;
+            }
+         }
+
+         Util.swap(array, leftIndex, indiceMenor);
+         leftIndex++;
+
+      }
+
+   }
+
+}
